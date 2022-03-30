14268ac3daee5dc80faaf92be947b6f46cb99929
dasunb <shalila.b@gmail.com>
2022-03-30 13:21:12 +0530

    Bug fixes to train.py

--- train.py
+++ train.py
@@ -142,7 +142,7 @@ if __name__ == '__main__':
         shared_array_y = pickle.load(filehandle_3)
 
     print("Loading Unigram table...")
-    with (open(path+"unigram_table_3", 'rb')) as filehandle_4:
+    with (open(path+"unigram_table", 'rb')) as filehandle_4:
         unigram_table = pickle.load(filehandle_4)
 
     unigram_table = unigram_table.astype(np.int32)
